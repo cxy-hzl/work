01ddf6588eb79f0bdf3f69f599c58d02c7aaee2f
houzilin <1959873847@qq.com>
2018-03-23 13:46:04 +0800

    添加新的一行到里边

---
+++ workerman/adds.php
@@ -0,0 +1,26 @@
+<?php 
+use Workerman\Worker;
+require_once __DIR__ . '/vendor/autoload.php';
+
+// 注意：这里与上个例子不通，使用的是websocket协议
+$ws_worker = new Worker("websocket://0.0.0.0:2000");
+
+// 启动4个进程对外提供服务
+$ws_worker->count = 4;
+
+// 当收到客户端发来的数据后返回hello $data给客户端
+$ws_worker->onMessage = function($connection, $data)
+{
+    // 向客户端发送hello $data
+
+    global $ws_worker;
+    foreach($ws_worker->connections as $connection)
+    {
+
+        $connection->send($data);
+    }
+
+};
+
+// 运行worker
+Worker::runAll();
